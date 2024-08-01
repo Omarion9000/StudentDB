621f54d7ecf1fb223eb781738fc5a07e4ff62287
Omarion9000 <omarion.montoya@gmail.com>
2024-07-31 23:13:23 -0400

    Agregar archivos de proyecto.

---
+++ StudentDB/Controllers/StudentsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentManagement.Data;
+using StudentManagement.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentManagement.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public StudentsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Students.ToListAsync());
+        }
+
+        // Other actions (Create, Edit, Delete) can be added here
+    }
+}
